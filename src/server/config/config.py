267b5b6fc527b3c2aea19fa845b27c682ff14beb
Adam Amster <adam.amster@alleninstitute.org>
2021-10-24 21:00:55 -0400

    Remove config file from repo

--- src/server/config/config.py
+++ src/server/config/config.py
@@ -1,6 +1,9 @@
+import uuid
+
 ARTIFACT_DB_PATH = "/allen/aibs/informatics/segmentation_eval_dbs" \
                    "/ssf_mouse_id_409828.db"
 SQLALCHEMY_DATABASE_URI = 'sqlite:////allen/aibs/informatics/aamster' \
                           '/cell_labeling_app.db'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 ARTIFACT_DIR = '/allen/aibs/informatics/danielsf/classifier_prototype_data'
+SESSION_SECRET_KEY = str(uuid.uuid4())
