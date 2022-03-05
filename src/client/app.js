a626c859124fa33a4266f9625b3b6de75a5fb7b0
Adam Amster <adam.amster@alleninstitute.org>
2022-03-05 14:00:57 -0500

    Move displaying region meta to where a new region is loaded

--- src/client/app.js
+++ src/client/app.js
@@ -290,8 +290,6 @@ class CellLabelingApp {
             $('button#projection_contrast_reset').attr('disabled', false);
             $('input#projection_contrast_low_quantile').attr('disabled', false);
             $('input#projection_contrast_high_quantile').attr('disabled', false);
-            $('#region_meta').html(
-                `Experiment id: ${this.experiment_id} | Region id: ${this.region['id']}`)
 
             await this.updateShapesOnProjection();
 
@@ -469,6 +467,8 @@ class CellLabelingApp {
         const region = await this.getRandomRegionFromRandomExperiment()
             .then(region => {
                 this.is_loading_new_region = false;
+                $('#region_meta').html(
+                    `Experiment id: ${this.experiment_id} | Region id: ${this.region['id']}`);
                 return region;
             })
             .catch(() => {
