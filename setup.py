cdba0402db49a16da523880320aab08943b7564a
aamster <adam.amster@alleninstitute.org>
2022-04-25 20:53:23 -0400

    Install dependencies in setup

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 from setuptools import setup, find_packages
 
+requirements = [r.strip() for r in open('requirements.txt').readlines()]
+
 setup(
       name="cell_labeling_app",
       description="Web app to label ROIs in Ophys movies",
@@ -7,5 +9,6 @@ setup(
       author_email="adam.amster@alleninstitute.org",
       url="https://github.com/AllenInstitute/cell_labeling_app",
       package_dir={"": "src/server"},
-      packages=find_packages(where="src/server")
+      packages=find_packages(where="src/server"),
+      install_requires=requirements
 )
